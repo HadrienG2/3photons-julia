eac45b83235552988a9608ee8ea9d330adbeca1d
Hadrien Grasland <grasland@lal.in2p3.fr>
2020-02-24 16:05:12 +0100

    Finish the matrix elements computation

--- ResCont.jl
+++ ResCont.jl
@@ -13,7 +13,7 @@ using ..EvGen: Event, NUM_OUTGOING
 using ..Numeric: Float
 using ..Spinor: 𝛼_amp, 𝛽₊_amp, 𝛽₋_amp, MMM, NUM_HELICITIES, PhotonHelicities,
                 PPP, SpinorProducts
-using StaticArrays: SMatrix, SVector
+using StaticArrays: SMatrix, SVector, @SMatrix
 
 export NUM_RESULTS, ResultContibution, ResultVector
 
@@ -23,6 +23,21 @@ const NUM_RESULTS = 5
 
 "Storage for matrix elements"
 const ResultVector = SVector{NUM_RESULTS, Float}
+
+"Index of the electromagnetic matrix element"
+const A = 0
+
+"Index of the positive electroweak matrix element"
+const B₊ = 1
+
+"Index of the negative electroweak matrix element"
+const B₋ = 2
+
+"Index of the real part of the mixed matrix element"
+const R_MX = 3
+
+"Index of the imaginary part of the mixed matrix element"
+const I_MX = 4
 
 
 "Array of square matrix elements contribution with detail of helicities"
@@ -44,9 +59,25 @@ function ResultContribution(couplings::Couplings, event::Event)
     𝛼_amps = map(hel-> 𝛼_amp(spinor, hel) * couplings.g_𝛼, hels)
     𝛽₊_amps = map(hel -> 𝛽₊_amp(spinor, hel) * couplings.g_𝛽₊, hels)
     𝛽₋_amps = map(hel -> 𝛽₋_amp(spinor, hel) * couplings.g_𝛽₋, hels)
+    mixed_amps = 2 .* 𝛼_amps .* conj(𝛽₊_amps)
 
-    # TODO: Not implemented yet
-    throw(AssertionError("Not implemented yet"))
+    # Compute the matrix elements
+    @SMatrix [
+        if contrib == A
+            abs2(𝛼_amps[hel])
+        elseif contrib == B₊
+            abs2(𝛽₊_amps[hel])
+        elseif contrib == B₋
+            abs2(𝛽₋_amps[hel])
+        elseif contrib == R_MX
+            real(mixed_amps[hel])
+        elseif contrib == I_MX
+            imag(mixed_amps[hel])
+        else
+            throw(AssertionError("Unexpected contribution"))
+        end
+        for contrib=A:I_MX, hel=1:NUM_HELICITIES
+    ]
 end
 
 end
