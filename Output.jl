b0e3b0ebe71d925e2bdf316fb814e62a9ad0a866
Hadrien Grasland <grasland@lal.in2p3.fr>
2020-02-27 11:27:48 +0100

    Start fleshing out file output

--- Output.jl
+++ Output.jl
@@ -9,6 +9,8 @@ standard output and various files
 """
 module Output
 
+import Dates
+
 using ..Config: Configuration
 using ..Numeric: Float
 using ..ResFin: FinalResults, print_eric, print_fawzi
@@ -23,9 +25,43 @@ function dump_results(cfg::Configuration,
     # Print out some final results on stdout
     print_eric(res_fin)
     print_fawzi(res_fin)
+
+    # Compute a timestamp of when the run ended
+    current_time = Dates.now()
+    timestamp = Dates.format(current_time, "dd-uuu-yy   HH:MM:SS")
+
+    # So, apparently, Julia has println but not writeln because... reasons?
+    writeln(f, s) = write(f, s*'\n')
+
+    # Write execution timings to a file
+    open("res.times", "w") do tim_file
+        # Write a timestamp of when the run ended
+        writeln(tim_file, " $(timestamp)")
+
+        # Write program performance stats
+        writeln(tim_file, " ---------------------------------------------")
+        writeln(tim_file, " Temps ecoule                   : ???")
+        writeln(tim_file, " Temps ecoule utilisateur       : $(elapsed_secs)")
+        writeln(tim_file, " Temps ecoule systeme           : ???")
+        secs_per_ev = elapsed_secs / cfg.num_events
+        writeln(tim_file, " Temps ecoule par evenement     : $(secs_per_ev)")
+    end
+
+    # Write main results file. Try to mimick the original C++ format as well as
+    # possible to ease comparisons, even where it makes little sense.
+    open("res.data", "w") do dat_file
+        # TODO: Finish translating the program
+        throw(AssertionError("Not implemented yet"))
+    end
     
-    # TODO: Finish translating the program
-    throw(AssertionError("Not implemented yet"))
+    # Append the results of this run to a cumulative file
+    #
+    # NOTE: This part is completely broken in the C++ version, I did my best to
+    #       fix it in this version.
+    open("pil.mc", "a") do cum_dat_file
+        # TODO: Finish translating the program
+        throw(AssertionError("Not implemented yet"))
+    end
 end
 
 end
